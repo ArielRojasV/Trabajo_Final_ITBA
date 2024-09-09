515aa770896957be2b63084963bbe8afea3cd5b1
ArielRojasV <arielroj.22@gmail.com>
2024-09-08 23:38:02 -0300

    Carga inicial de proyecto

---
+++ pipeline/extraccion.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+url = "https://iol.invertironline.com/Titulo/DatosHistoricos?simbolo=YPFD&mercado=BCBA" 
+df_ypf_ext = pd.read_html(url)
+
+df_ypl = df_ypf_ext[1]
+
+print(df_ypl)
+
+    
+
+    req = requests.get(url)
---
+++ pipeline/tests/test_read_html.py
@@ -0,0 +1,13 @@
+
+import requests
+
+def test_conexion():
+   
+    url = "https://iol.invertironline.com/Titulo/DatosHistoricos?simbolo=YPFD&mercado=BCBA" 
+
+    req = requests.get(url)
+ 
+    assert req.status_code == 200  # Validacion de estado de conexion
+
+
+    
