ace5fcd97ee8e99d51be4bd618a3a0bff056db1c
ArielRojasV <arielroj.22@gmail.com>
2024-10-01 19:28:51 -0300

    Correcion Tests

--- pipeline/tests/test_read_html.py
+++ pipeline/tests/test_read_html.py
@@ -1,19 +1,15 @@
 
 import requests
-import config_extraccion as confext
 from datetime import datetime
+
 
 def test_conexion():
    
     url = "https://iol.invertironline.com/Titulo/DatosHistoricos?simbolo=YPFD&mercado=BCBA" 
 
-    ##req = requests.get(url)
- 
-    ##assert req.status_code == 200  # Validacion de estado de conexion
-
     response = requests.get(url)
-    if response.ok:   # alternatively you can use response.status_code == 200
-        print("PaginaAccesible")
+    if response.ok:   
+        print("Pagina Accesible")
     else:
         print(f"Fallo, no se establecio conexion. Codigo : {response.status_code}")
 
@@ -21,10 +17,12 @@ def test_conexion():
 
 def test_conexion_BCRA():
 
-    response = requests.get(confext.obtener_datos_BCRA( confext.obtener_url_moneda_fechas("cotizaciones" , "USD" , datetime.today().strftime('%Y-%m-%d')  ,  datetime.today().strftime('%Y-%m-%d') ))) 
+    url = "https://api.bcra.gob.ar/estadisticascambiarias/v1.0/Cotizaciones?fecha=" + datetime.today().strftime('%Y-%m-%d')
 
-    if response.ok:   # alternatively you can use response.status_code == 200
-        print("PaginaAccesible")
+    response = requests.get(url)
+
+    if response.ok:   
+        print("Pagina Accesible")
     else:
         print(f"Fallo, no se establecio conexion. Codigo : {response.status_code}")
 
