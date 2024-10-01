506660eb53dc95e8d28358d19272ea91fe6ff0c6
ArielRojasV <arielroj.22@gmail.com>
2024-10-01 09:08:45 -0300

    Reordeno metodos

--- pipeline/carga_bd.py
+++ pipeline/carga_bd.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-
+import pandas as pd
 
 #################Variables de Conexion a Base de Datos##########################
 redshift_user = "2024_ariel_rojas"
@@ -14,6 +14,28 @@ database = "pda"
 connection_string = f"postgresql://{redshift_user}:{redshift_pass}@{redshift_endpoint}:{port}/{database}"
 
 
+def get_codigo_acciones(): 	
+
+    engine = create_engine(connection_string)
+
+    try:         
+        connection = engine.raw_connection()   
+        cursor = connection.cursor()
+
+		#LLamo a tabla
+        cursor.execute("""select desc_sigla from "2024_ariel_rojas_schema".lk_accion where id_flg_activo = 'S'""")   
+        result = cursor.fetchall() 
+
+        return [i[0].rstrip() for i in result] 
+
+    finally: 
+		
+		#Cierro conexion 
+        if connection: 
+            cursor.close() 
+            connection.close()   
+
+
 ## Carga datos a las tablas
 def carga_dtf_to_bd(df, table): 
     
@@ -21,8 +43,7 @@ def carga_dtf_to_bd(df, table):
     
     try:
         with engine.connect() as connection:
-            print("Conexion Exitosa")
-            
+            print("Conexion Exitosa")            
             df.to_sql(name= table, con=engine, schema='2024_ariel_rojas_schema', if_exists='append', index=False)
 
     except Exception as e:
@@ -32,52 +53,52 @@ def carga_dtf_to_bd(df, table):
 def actualizar_ft_cotizaciones_bd():
     
     engine = create_engine(connection_string)
-    conn = engine.raw_connection()   
-    cursor = conn.cursor()
+    connection = engine.raw_connection()   
+    cursor = connection.cursor()
  
     with cursor:
         cursor.execute('CALL "2024_ariel_rojas_schema".sp_ft_cotizaciones_add()')
-        conn.commit()
+        connection.commit()
         cursor.close()  
-        conn.close() 
+        connection.close() 
      
 
 def actualizar_stg_cotizaciones_monedas_bd():
     
     engine = create_engine(connection_string)
-    conn = engine.raw_connection()   
-    cursor = conn.cursor()
+    connection = engine.raw_connection()   
+    cursor = connection.cursor()
  
     with cursor:
         cursor.execute('CALL "2024_ariel_rojas_schema".sp_stg_cotizaciones_monedas_add()')
-        conn.commit()
+        connection.commit()
         cursor.close()  
-        conn.close() 
+        connection.close() 
     
 
 def actualizar_stg_cotizaciones_acciones_bd():
     
     engine = create_engine(connection_string)
-    conn = engine.raw_connection()   
-    cursor = conn.cursor()
+    connection = engine.raw_connection()   
+    cursor = connection.cursor()
  
     with cursor:
         cursor.execute('CALL "2024_ariel_rojas_schema".sp_stg_cotizaciones_acciones_add()')
-        conn.commit()
+        connection.commit()
         cursor.close()  
-        conn.close()  
+        connection.close()  
 
 
 def actualizar_lk_cotizacion_monedas_bd():
     
     engine = create_engine(connection_string)
-    conn = engine.raw_connection()   
-    cursor = conn.cursor()
+    connection = engine.raw_connection()   
+    cursor = connection.cursor()
  
     with cursor:
         cursor.execute('CALL "2024_ariel_rojas_schema".sp_lk_cotizacion_monedas_add()')
-        conn.commit()
+        connection.commit()
         cursor.close()  
-        conn.close() 
+        connection.close() 
 
 
