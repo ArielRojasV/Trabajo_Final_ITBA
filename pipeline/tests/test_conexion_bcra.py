c8499cc9623659497eebc98c712ffcd1f19af3f8
ArielRojasV <arielroj.22@gmail.com>
2024-10-17 21:17:06 -0300

    Correcion test conexion BCRA

--- pipeline/tests/test_conexion_bcra.py
+++ pipeline/tests/test_conexion_bcra.py
@@ -1,15 +1,29 @@
-
 import requests
-from datetime import datetime
+import unittest
+import json
+from unittest.mock import patch, Mock, MagicMock
 
 
-def test_conexion_BCRA():
+def get_data_bcra():
+    response = requests.get("https://api.bcra.gob.ar/estadisticascambiarias/v1.0/Cotizaciones?" )    
+    data = json.loads(response.text)
+    return data
 
-    url = "https://api.bcra.gob.ar/estadisticascambiarias/v1.0/Cotizaciones?fecha=" + datetime.today().strftime('%Y-%m-%d')
+class Test_Get_Data(unittest.TestCase):
+    @patch('main.get_data_bcra')
+    def test_get_data_bcra(self, mock_get_data):
+        mock_data = {"codigoMoneda":"USD",
+                    "descripcion":"DOLAR E.E.U.U.",
+                    "tipoPase":0.00000000,
+                    "tipoCotizacion":1000}
+        
+        mock_get_data.return_value = Mock()
 
-    response = requests.get(url)
+        mock_get_data.return_value.json.return_value = mock_data
+        mock_get_data.return_value.status_code = 200
 
-    if response.ok:   
-        print("Pagina Accesible")
-    else:
-        print(f"Fallo, no se establecio conexion. Codigo : {response.status_code}")
+        result = getdata()
+
+        self.assertEqual(result,mock_data)
+
+
