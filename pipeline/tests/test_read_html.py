f170ce62d5fc6734fcf3e4b615e9498af29df87a
ArielRojasV <arielroj.22@gmail.com>
2024-10-17 23:16:18 -0300

    Correcion Test

--- pipeline/tests/test_read_html.py
+++ pipeline/tests/test_read_html.py
@@ -1,30 +1,35 @@
-
+import unittest
+from unittest.mock import patch 
+from typing import Dict
 import requests
-from datetime import datetime
 
 
-def test_conexion():
-   
-    url = "https://iol.invertironline.com/Titulo/DatosHistoricos?simbolo=YPFD&mercado=BCBA" 
-
+def get_data_Json(url: str) -> Dict[str, str]:
     response = requests.get(url)
-    if response.ok:   
-        print("Pagina Accesible")
-    else:
-        print(f"Fallo, no se establecio conexion. Codigo : {response.status_code}")
-
-       
-
-def test_conexion_BCRA():
-
-    url = "https://api.bcra.gob.ar/estadisticascambiarias/v1.0/Cotizaciones?fecha=" + datetime.today().strftime('%Y-%m-%d')
-
-    response = requests.get(url)
-
-    if response.ok:   
-        print("Pagina Accesible")
-    else:
-        print(f"Fallo, no se establecio conexion. Codigo : {response.status_code}")
+    return response.json()
 
 
+class Test_Get_Data_Json(unittest.TestCase):
+    @patch('requests.get')
+    def test_get_data(self, mock_get):
+        mock_response = mock_get.return_value  
+        mock_response.json.return_value = {'fechacotizacion': '01/01/2020',
+                                            'cierre' : '1000'}  
+        
+        url = "https://iol.invertironline.com/Titulo/DatosHistoricos?simbolo=YPFD&mercado=BCBA" 
+        result =  get_data_Json(url)  
+        
+        self.assertEqual(result, {'fechacotizacion': '01/01/2020',
+                                            'cierre' : '1000'})  
+        mock_get.assert_called_once_with(url)  
 
+
+"""
+apertura
+máximo
+mínimo
+cierre
+cierreajustado
+volumenmonto
+volumennominal
+"""
