780e1d2d1db0bbb4610db78b21513013d28712ea
ArielRojasV <arielroj.22@gmail.com>
2024-10-17 22:27:20 -0300

    Corrijo test conexion - OK

--- pipeline/tests/test_conexion_bcra.py
+++ pipeline/tests/test_conexion_bcra.py
@@ -1,29 +1,25 @@
+from unittest import TestCase
+from unittest.mock import patch 
 import requests
-import unittest
-import json
-from unittest.mock import patch, Mock, MagicMock
 
 
-def get_data_bcra():
-    response = requests.get("https://api.bcra.gob.ar/estadisticascambiarias/v1.0/Cotizaciones?" )    
-    data = json.loads(response.text)
-    return data
-
-class Test_Get_Data(unittest.TestCase):
-    @patch('main.get_data_bcra')
-    def test_get_data_bcra(self, mock_get_data):
-        mock_data = {"codigoMoneda":"USD",
-                    "descripcion":"DOLAR E.E.U.U.",
-                    "tipoPase":0.00000000,
-                    "tipoCotizacion":1000}
-        
-        mock_get_data.return_value = Mock()
-
-        mock_get_data.return_value.json.return_value = mock_data
-        mock_get_data.return_value.status_code = 200
-
-        result = getdata()
-
-        self.assertEqual(result,mock_data)
+def url_exists(url):
+    r = requests.get(url)
+    if r.status_code == 200:
+        return True
+    elif r.status_code == 404:
+        return False
 
 
+class Test_Get_Data(TestCase):
+    def test_return_true(self):
+        with patch('requests.get') as mock_request:
+            url = "https://api.bcra.gob.ar/estadisticascambiarias/v1.0/Cotizaciones?" 
+
+            mock_request.return_value.status_code = 200
+
+            self.assertTrue(url_exists(url))
+
+            mock_request.assert_called_once_with(url)
+
+ 
