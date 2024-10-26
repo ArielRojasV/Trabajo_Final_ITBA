b8eba15294c0b1a56a3d148ffeb2e70567653da7
ArielRojasV <arielroj.22@gmail.com>
2024-10-26 19:54:50 -0300

    Modificacion Tests

--- pipeline/tests/test_conexion_bcra.py
+++ pipeline/tests/test_conexion_bcra.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from unittest.mock import patch 
+from unittest.mock import patch   
 import requests
 
 
@@ -10,11 +10,11 @@ def url_exists(url):
     elif r.status_code == 404:
         return False
 
-
+# Test para ver si url responde
 class Test_Get_Data(TestCase):
     def test_return_true(self):
         with patch('requests.get') as mock_request:
-            url = "https://api.bcra.gob.ar/estadisticascambiarias/v1.0/Cotizaciones?" 
+            url = "https://api.bcra.gob.ar/" 
 
             mock_request.return_value.status_code = 200
 
